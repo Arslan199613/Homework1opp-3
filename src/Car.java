39334447ea0f0a1e87f10ddc3edc0d403ea63e3f
Samsung <arslan.zakirov.1996@mail.ru>
2022-11-14 18:18:04 +0300

    Домашнее задание №1 по ООП (3 задача) с испльзованием Конструктора

--- src/Car.java
+++ src/Car.java
@@ -1,13 +1,22 @@
 public class Car {
     public String brand;
     public String model;
-    public String engineVolume;
+    public double engineVolume;
     public String color;
     public int year;
     public String country;
 
+    public Car(String brand,String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+
+    }
     void tellTheCar() {
-        System.out.println(brand+","+ year+" года выпуска, сборка - "+country+", цвет -"+ color+", объем двигателя-"+engineVolume);
+        System.out.println(brand+" "+ model+","+ year+" года выпуска, сборка - "+country+", цвет -"+ color+", объем двигателя-"+engineVolume+".");
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Car lada = new Car();
-        lada.brand = "Lada Granta";
-        lada.year = 2015;
-        lada.country = "Россия";
-        lada.color = "желтый";
-        lada.engineVolume = "1,7 л.";
+        Car lada = new Car("Lada","Granta",1.7,"желтый",2015,"Россия");
+        Car audi = new Car("Audi","A8 50 L TDI quattro",3.0,"черный",2020,"Германия");
+        Car bmw = new Car("BMW","Z8",3.0,"черный",2021,"Германия");
+        Car kia = new Car("Kia","Sportage 4-го поколения",2.4,"красный",2018,"Южная Корея");
+        Car hyundai = new Car("Hyundai","Avante",1.6,"оранжевый",2016,"Южная Корея");
+
         lada.tellTheCar();
-
-        Car audi = new Car();
-        audi.brand = "Audi A8 50 L TDI quattro";
-        audi.year = 2020;
-        audi.country = "Германия";
-        audi.color = "черный";
-        audi.engineVolume = "3,0 л.";
         audi.tellTheCar();
-
-        Car bmw = new Car();
-        bmw.brand = "BMW Z8";
-        bmw.year = 2021;
-        bmw.country = "Германия";
-        bmw.color = "черный";
-        bmw.engineVolume = "3,0 л.";
-        audi.tellTheCar();
-
-        Car kia = new Car();
-        kia.brand = "Kia Sportage 4-го поколения";
-        kia.year = 2018;
-        kia.country = "Южная Корея";
-        kia.color = "красный";
-        kia.engineVolume = "2,4 л.";
+        bmw.tellTheCar();
         kia.tellTheCar();
-
-        Car hyundai = new Car();
-        hyundai.brand = "Hyundai Avante";
-        hyundai.country = "Южная Корея";
-        hyundai.color = "оранжевый";
-        hyundai.engineVolume = "1,6 л.";
-        hyundai.year = 2016;
         hyundai.tellTheCar();
+
     }
 }
